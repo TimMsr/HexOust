c5836e5d7f3b61c20e21861c01bb2889b5cb080c
Tim M <tim.maeser2@gmail.com>
2025-02-19 12:13:55 +0000

    Update Hexagon.java

--- Hexagon.java
+++ Hexagon.java
@@ -1,5 +1,38 @@
 public class Hexagon {
+    private int q;
+    private int r;
+    private int s;
+
     public static void main(String[] args) {
         
     }
+
+    public add(Hexagon a) {
+        return new Hexagon(q,r,s);
+    }
+
+    public remove(Hexagon a) {
+        
+    }
+
+    public boolean isAdjacent(Hexagon a) {
+        if(q == a.q) {
+            if(r == a.r+1 || r == a.r-1) {
+                return true;
+            }
+        }
+        if(r == a.r) {
+            if(q == a.q+1 || q == a.q-1) {
+                return true;
+            }
+        }
+        if(s == a.s) {
+            if(q == a.q+1 || q == a.q-1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
