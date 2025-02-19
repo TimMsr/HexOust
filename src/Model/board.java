0d51cf147651d1de90efc4a95ee4960adf2cba41
dan-rodin <161745536+dan-rodin@users.noreply.github.com>
2025-02-19 21:33:39 +0000

    Project is modular and follows MVC architecture

---
+++ src/Model/board.java
@@ -0,0 +1,29 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class board {
+    private ArrayList<ArrayList<Hexagon>> hexagons;
+    private ArrayList<ArrayList<Hexagon>> redGroups;
+    private ArrayList<ArrayList<Hexagon>> blueGroups;
+
+    public board() {
+        hexagons = new ArrayList<ArrayList<Hexagon>>();
+        redGroups = new ArrayList<ArrayList<Hexagon>>();
+        blueGroups = new ArrayList<ArrayList<Hexagon>>();
+    }
+
+    public ArrayList<ArrayList<Hexagon>> getRedGroups() {
+        return redGroups;
+    }
+
+    public ArrayList<ArrayList<Hexagon>> getBlueGroups() {
+        return blueGroups;
+    }
+
+    
+    public static void main(String[] args) {
+        
+
+    }
+}
