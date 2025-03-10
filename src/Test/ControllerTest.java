baca177651fe4f2e87cad32136ed66f65002a9cc
Hubert Krzemieniewski <hubert.krzemieniewski@ucdconnect.ie>
2025-03-10 23:29:34 +0000

    Added test cases for HandleMove method

--- src/Test/ControllerTest.java
+++ src/Test/ControllerTest.java
@@ -60,4 +60,48 @@ public class ControllerTest {
         assertTrue(dummyGUI.updateCalled);
     }
 
+    @Test
+    void testHandleMoveSwitchesPlayers() {
+        Controller c = new Controller();
+        Hexagon h1 = new Hexagon(0,0, 0);
+        Hexagon h2 = new Hexagon(0, 1, -1);
+
+        // HandleMove changes turn when valid move is made
+        assertEquals("RED", c.getCurrentPlayer());
+        c.handleMove(h1);
+        assertEquals("BLUE", c.getCurrentPlayer());
+        c.handleMove(h2);
+        assertEquals("RED", c.getCurrentPlayer());
+    }
+
+    @Test
+    void testHandleMoveAssignsOwnerToHexagon() {
+        Controller c = new Controller();
+        Hexagon h1 = new Hexagon(0,0, 0);
+        Hexagon h2 = new Hexagon(0, 1, -1);
+
+        // Assigns owners to hexagons
+        c.handleMove(h1);
+        assertEquals("RED", h1.getOwner());
+        c.handleMove(h2);
+        assertEquals("BLUE", h2.getOwner());
+    }
+
+    @Test
+    void testHandleMoveDuplicates() {
+        Controller c = new Controller();
+        Hexagon h1 = new Hexagon(0,0, 0);
+
+        // Passing a duplicate hexagon should throw an IllegalArgumentException
+        c.handleMove(h1);
+        assertThrows(IllegalArgumentException.class, () -> c.handleMove(h1));
+    }
+
+    @Test
+    void testHandleMoveNull() {
+        Controller c = new Controller();
+
+        // Passing a null value should throw an NullPointerException
+        assertThrows(NullPointerException.class, () -> c.handleMove(null));
+    }
 }
